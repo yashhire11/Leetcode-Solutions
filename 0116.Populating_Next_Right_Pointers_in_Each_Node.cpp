da1cd30e8b14effebfd6c33284d2028aac8ecd30
Yash Hire <hireyash85@gmail.com>
2024-06-22 22:59:22 +0530

    Create 0116.Populating_Next_Right_Pointers_in_Each_Node.cpp

---
+++ 0116.Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL){
+            return NULL;
+        }
+        Node* current=root;
+        while(current->left!=NULL){
+            
+            Node* temp=current;
+            while(current!=NULL){
+                
+                current->left->next=current->right;
+                current->right->next=current->next==NULL?NULL: current->next->left;
+                current=current->next;
+                
+            }
+            current=temp->left;
+        }
+        return root;
+        
+    }
+};
