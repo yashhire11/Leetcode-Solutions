783bbbf1d8935a8aed466dab07dd7a2146e5ad16
Yash Hire <hireyash85@gmail.com>
2024-06-26 09:20:30 +0530

    Create 0155.Min_Stack.java

---
+++ 0155.Min_Stack.java
@@ -0,0 +1,31 @@
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || val<=minStack.peek()){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int val = stack.pop();
+        if(val == minStack.peek()){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
