a6387a093a975f93630753449c86df3d7c750687
Yash Hire <hireyash85@gmail.com>
2024-06-28 19:40:40 +0530

    Create 1137.N-th_Tribonacci_Number.cpp

---
+++ 1137.N-th_Tribonacci_Number.cpp
@@ -0,0 +1,23 @@
+//T.C : Without Memoization - O(3^n) and with Memoization - o(n)
+//S.C : O(n) (But size of t is 38 which is constant hence O(1))
+class Solution {
+public:
+    int t[38];
+    int find(const int& n) {
+        if(n == 0)           return 0;
+        if(n == 1 || n == 2) return 1;
+        if(t[n] != -1)       return t[n];
+        
+        int a = find(n-1);
+        int b = find(n-2);
+        int c = find(n-3);
+            
+        
+        return t[n] = a+b+c;
+    }
+    int tribonacci(int n) {
+        memset(t, -1, sizeof(t));
+        
+        return find(n);
+    }
+};
