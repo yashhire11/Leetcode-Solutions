c7845a60429f9b207951a99bd1706aba097a7148
Yash Hire <hireyash85@gmail.com>
2024-06-28 19:03:43 +0530

    Create 0605.Can_Place_Flowers.cpp

---
+++ 0605.Can_Place_Flowers.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        
+        int l = flowerbed.size();
+        
+        if(n == 0)
+            return true;
+        
+        
+        for(int i = 0; i<l; i++) {
+            
+            if(flowerbed[i] == 0) {
+                
+                bool leftEmpty   = (i == 0) || (flowerbed[i-1] == 0);
+                
+                bool rightEmpty  = (i == l-1) || (flowerbed[i+1] == 0);
+                
+                if(leftEmpty && rightEmpty) {
+                    flowerbed[i] = 1;
+                    n--;
+                    if(n == 0)
+                        return true;
+                }
+                
+            }
+            
+        }
+        
+        return false;
+        
+    }
+};
